d45d7ca2e9c5124fd7ed174c116cda6eeb282999
Emílio Silva <emiliosarges@gmail.com>
2025-01-17 16:26:43 -0300

    desestruturação

--- procurar-em-lista2.js
+++ procurar-em-lista2.js
@@ -4,9 +4,10 @@ const medias = [10, 8, 7.5, 9];
 const lista = [alunos, medias];
 
 function exibeNomeENota(aluno) {
-    if (lista[0].includes(aluno)) {     
-        const indice = lista[0].indexOf(aluno); //includes() retorba true ou false
-        const mediaAluno = lista[1][indice];
+    if (lista[0].includes(aluno)) {   
+        const [alunos, medias] = lista;   //desestruturação  
+        const indice = alunos.indexOf(aluno); 
+        const mediaAluno = medias[indice];
         return `${aluno} tem média ${mediaAluno}`;
     } else {
         return `Aluno ${aluno} não está na lista`;
