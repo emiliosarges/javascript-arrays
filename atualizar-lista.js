a93dcdc0e0e069c694511bb093f84c4f1134eefe
Emílio Silva <emiliosarges@gmail.com>
2025-01-16 23:47:54 -0300

    array.concat()

--- atualizar-lista.js
+++ atualizar-lista.js
@@ -1,6 +1,6 @@
 const listaEstudantes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
 
-listaEstudantes.splice(1, 2, 'Rodrigo', 'José', 'Paula');
+listaEstudantes.splice(1, 0, 'Rodrigo', 'José', 'Paula');
 
 console.log(listaEstudantes);
 
---
+++ juntar-salas.js
@@ -0,0 +1,5 @@
+const salasJS = ['Evaldo', 'Camis', 'Mari'];
+const salaPython = ['Ju', 'Leo', 'Raquel'];
+
+const salasUnificadas = salasJS.concat(salaPython);
+console.log(salasUnificadas);
