8ff5abf3f578638a17c6dcdc4696ef01463bbf90
Emílio Silva <emiliosarges@gmail.com>
2025-01-17 14:41:05 -0300

    Entendendo o includes()

---
+++ procurar-em-lista.js
@@ -0,0 +1,18 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+const medias = [10, 8, 7.5, 9];
+
+const lista = [alunos, medias];
+
+function exibeNomeENota(aluno) {
+    if (lista[0].includes(aluno)) {                 //includes() retorba true ou false
+        const indice = lista[0].indexOf(aluno);
+        const mediaAluno = lista[1][indice];
+        return `${aluno} tem média ${mediaAluno}`;
+    } else {
+        return `Aluno ${aluno} não está na lista`;
+    }
+}
+
+console.log(exibeNomeENota('Juliana'));
+console.log(exibeNomeENota('Caio'));
+console.log(exibeNomeENota('Carlos'));
