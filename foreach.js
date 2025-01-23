48ba68ff0412003c0730247dade1235209de34c9
Emílio Sarges <emiliosarges@gmail.com>
2025-01-23 13:35:08 -0300

    map.js

--- foreach.js
+++ foreach.js
@@ -2,9 +2,17 @@ const notas = [10, 6.5, 8, 7.5];
 
 let somaDasNotas = 0;
 
-notas.forEach(function (nota) {
+// notas.forEach(function (nota) {
+//     somaDasNotas += nota;
+// })
+
+notas.forEach(somaNotas);
+
+function somaNotas(nota) {
     somaDasNotas += nota;
-})
+}
+
+
 
 const media = somaDasNotas / notas.length;
 
